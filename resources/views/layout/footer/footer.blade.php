0d9a08848a305216eb8ce4ab8704b5c75d696191
dipankarkataki <dipankarkataki@gmail.com>
2025-03-30 00:42:57 +0530

    first commit

---
+++ resources/views/layout/footer/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer flex flex-auto items-center h-16 px-4 sm:px-6 md:px-8">
+    <div class="flex items-center justify-between flex-auto w-full">
+        <span>Copyright © @php echo date('Y'); @endphp 
+        <span class="font-semibold">Swagat Hospital</span> All rights reserved.</span>
+    </div>
+</footer>
