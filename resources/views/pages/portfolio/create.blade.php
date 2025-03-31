1c222e07fda310b0feb13c3d42ed7d28699cd0a1
dipankarkataki <dipankarkataki@gmail.com>
2025-03-31 16:54:25 +0530

    add more and remove expertise functionality created

--- resources/views/pages/portfolio/create.blade.php
+++ resources/views/pages/portfolio/create.blade.php
@@ -13,17 +13,23 @@
                             <div class="form-item">
                                 <div class="grid xl:grid-cols-4">
                                     <div class="form-item vertical">
-                                        <label class="form-label mb-2">Profile Picture</label>
-                                        <div class="upload upload-draggable hover:border-primary-600 cursor-pointer h-[300px]">
+                                        <label class="form-label mb-2">Profile Picture *</label>
+                                        <div
+                                            class="upload upload-draggable hover:border-primary-600 cursor-pointer h-[300px]">
                                             <div>
-                                                <input class="upload-input draggable" name="uploadProfilePicture" type="file" title="" value="">
+                                                <input class="upload-input draggable" name="uploadProfilePicture"
+                                                    type="file" title="" value="" required>
                                             </div>
                                             <div class="text-center">
-                                                <svg class="mx-auto mb-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                                                </svg>                                      
+                                                <svg class="mx-auto mb-3" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                    class="size-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                                                </svg>
                                                 <p class="font-semibold">
-                                                    <span class="text-gray-800 dark:text-white">Drop your image here, or</span>
+                                                    <span class="text-gray-800 dark:text-white">Drop your image here,
+                                                        or</span>
                                                     <span class="text-blue-500">browse</span>
                                                 </p>
                                                 <p class="mt-1 opacity-60 dark:text-white">Support: jpeg, png</p>
@@ -33,36 +39,55 @@
                                 </div>
                             </div>
                             <div class="form-item">
-                                <label class="form-label mb-2">Required *</label>
+                                <label class="form-label mb-2">Full Name *</label>
                                 <div>
-                                    <input type="text" class="input form-control" name="inputRequired" placeholder="Required *">
+                                    <input type="text" class="input form-control" name="fullName"
+                                        placeholder="Required *" required>
                                 </div>
                             </div>
                             <div class="form-item">
-                                <label class="form-label mb-2">Min Length</label>
+                                <label class="form-label mb-2">Choose Years of Experience *</label>
                                 <div>
-                                    <input type="text" class="input form-control" name="inputMinLength" placeholder="Enter minimum 8 characters">
+                                    <select class="input" name="yearsOfExperience" required>
+                                        <option selected>Choose years</option>
+                                        @for ($i = 1; $i <= 60; $i++)
+                                            <option value="{{ $i }}">Years of Experience : {{ $i }}
+                                            </option>
+                                        @endfor
+                                    </select>
                                 </div>
                             </div>
                             <div class="form-item">
-                                <label class="form-label mb-2">Max Length</label>
+                                <label class="form-label mb-2">Choose Linked Department *</label>
                                 <div>
-                                    <input type="text" class="input form-control" name="inputMaxLength"
-                                        placeholder="Enter maximum 8 characters">
+                                    <select class="input" name="department" required>
+                                        <option selected>Choose department</option>
+                                        <option value="gynacology">Gynachology</option>
+                                        <option value="medicine">Medicine</option>
+                                    </select>
                                 </div>
                             </div>
                             <div class="form-item">
-                                <label class="form-label mb-2">Range length</label>
+                                <label class="form-label mb-2">Languages Speak</label>
                                 <div>
-                                    <input type="text" class="input form-control" name="inputRangeLength"
-                                        placeholder="Enter minimum 2 & maximum 6 characters" required>
+                                    <input type="text" class="input form-control" name="languagesSpeak"
+                                        placeholder="Enter languages separated by comma(,)">
                                 </div>
                             </div>
                             <div class="form-item">
-                                <label class="form-label mb-2">Min Value</label>
+                                <label class="form-label mb-2">Brief Description *</label>
                                 <div>
-                                    <input type="text" class="input form-control" name="inputMinValue"
-                                        placeholder="Enter number more than 8">
+                                    <textarea class="input input-textarea" placeholder="Write a brief description about the doctor, his work etc." required></textarea>
+                                </div>
+                            </div>
+                            <div class="form-item">
+                                <label class="form-label mb-2">Add Expertise</label>
+                                <div>
+                                    <div class="input-group mb-4">
+                                        <input class="input expertise" type="text" name="expertise[]" placeholder="e.g Expert in Robotic Surgery">
+                                        <button class="btn btn-solid" id="addExpertiseBtn"> + Add </button>
+                                    </div>
+                                    <div id="expertiseList"></div>
                                 </div>
                             </div>
                             <div class="form-item">
@@ -96,15 +121,15 @@
                             <div class="form-item">
                                 <label class="form-label mb-2">URL</label>
                                 <div>
-                                    <input type="url" class="input form-control" name="inputUrl" placeholder="Enter a valid URL"
-                                        required>
+                                    <input type="url" class="input form-control" name="inputUrl"
+                                        placeholder="Enter a valid URL" required>
                                 </div>
                             </div>
                             <div class="form-item">
                                 <label class="form-label mb-2">Digit</label>
                                 <div>
-                                    <input type="url" class="input form-control" name="inputDigit" placeholder="Enter a Digit"
-                                        required>
+                                    <input type="url" class="input form-control" name="inputDigit"
+                                        placeholder="Enter a Digit" required>
                                 </div>
                             </div>
                             <div class="form-item"><label class="form-label"></label>
@@ -123,93 +148,24 @@
 @endsection
 @section('custom-scripts')
     <script>
-        $('#form-validation').validate({
-            ignore: ':hidden:not(:checkbox)',
-            errorElement: 'div',
-            errorClass: 'input-invalid',
-            validClass: 'input-valid',
-            errorPlacement: function(error, element) {
-                error.addClass('text-red-500 mt-1');
-                error.removeClass('input-valid');
-                if (element.prop('type') === 'checkbox') {
-                    error.insertAfter(element.parent('label'));
-                } else if(element.prop('type') === 'file'){
-                    const uploadDiv = element.closest('.upload');
+        $(document).ready(function(){
+            $('#addExpertiseBtn').on('click', function(e){
+                e.preventDefault();
 
-                    error.insertAfter(uploadDiv);
-                    uploadDiv.css({
-                        'border': '2px dashed rgb(239 68 68)'
-                    });
-                } else {
-                    error.insertAfter(element);
-                }
-            },
-            rules: {
-                uploadProfilePicture:{
-                    required: true
-                },
+                $('#expertiseList').append(
+                    `
+                        <div class="input-group mb-4 expertise-item">
+                            <input class="input expertise" type="text" name="expertise[]" placeholder="e.g Expert in Robotic Surgery">
+                            <button class="btn bg-rose-600 hover:bg-rose-500 active:bg-rose-700 text-white removeExpertiseBtn"> - Remove </button>
+                        </div>
+                    `
+                )
+            });
 
-                inputRequired: {
-                    required: true
-                },
-                inputMinLength: {
-                    required: true,
-                    minlength: 6
-                },
-                inputMaxLength: {
-                    required: true,
-                    minlength: 8
-                },
-                inputUrl: {
-                    required: true,
-                    url: true
-                },
-                inputRangeLength: {
-                    required: true,
-                    rangelength: [2, 6]
-                },
-                inputMinValue: {
-                    required: true,
-                    min: 8
-                },
-                inputMaxValue: {
-                    required: true,
-                    max: 6
-                },
-                inputRangeValue: {
-                    required: true,
-                    max: 6,
-                    range: [6, 12]
-                },
-                inputEmail: {
-                    required: true,
-                    email: true
-                },
-                inputPassword: {
-                    required: true
-                },
-                inputPasswordConfirm: {
-                    required: true,
-                    equalTo: '#password'
-                },
-                inputDigit: {
-                    required: true,
-                    digits: true
-                },
-                inputValidCheckbox: {
-                    required: true
-                }
-            }
-        });
-
-        $('input[name="uploadProfilePicture"]').on('change', function() {
-            const uploadDiv = $(this).closest('.upload');
-
-            if ($(this).val()) {
-                uploadDiv.css({
-                    'border': ''  // Reset border
-                });
-            }
+            $(document).on("click", ".removeExpertiseBtn", function () {
+                $(this).parent(".expertise-item").remove();
+            });
+            
         });
     </script>
 @endsection
