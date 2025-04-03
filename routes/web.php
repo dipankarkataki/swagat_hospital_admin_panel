44b6b07a80def2bc4378571774dfec8cde89cd22
dipankarkataki <dipankarkataki@gmail.com>
2025-04-03 22:33:09 +0530

    routes middleware implemented

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,12 @@ use App\Http\Controllers\Portfolio\PortfolioController;
 
 Route::match(['get', 'post',], '/', [LoginController::class, 'login'])->name('login');
 
-Route::group(['prefix' => 'dashboard'], function(){
-    Route::get('', [DashboardController::class, 'index'])->name('dashboard.index');
-});
-
 Route::group(['middleware' => 'auth'], function(){
+    
+    Route::group(['prefix' => 'dashboard'], function(){
+        Route::get('', [DashboardController::class, 'index'])->name('dashboard.index');
+    });
+
     Route::group(['prefix' => 'portfolio'], function(){
         Route::get('list-of-doctors', [PortfolioController::class, 'listOfDoctors'])->name('portfolio.list');
         Route::match(['get', 'post'], 'create-doctors-portfolio', [PortfolioController::class, 'createDoctorsPortfolio'])->name('portfolio.create');
