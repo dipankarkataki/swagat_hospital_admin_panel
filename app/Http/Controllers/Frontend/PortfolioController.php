13fc39f7f35330b8bb66b314a3acd4b10d384707
dipankarkataki <dipankarkataki@gmail.com>
2025-05-05 22:28:13 +0530

    portfolio/list api created

---
+++ app/Http/Controllers/Frontend/PortfolioController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Models\Portfolio;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+use App\Traits\ApiResponse;
+
+class PortfolioController extends Controller
+{
+    use ApiResponse;
+
+    public function listOfDoctors(){
+
+        try{
+            $get_portfolio = Portfolio::with('hospital', 'departments')->latest()->get();
+            return $this->success('Portfolio fetched successfully', $get_portfolio, 200);
+        }catch(\Exception $e){
+            Log::error('Error occurred at create portfolio function: ' . $e->getMessage());
+            return $this->error('Oops! Something went wrong. Failed to fetch doctors portfolio.', null, 500);
+        }
+        
+    }
+}
