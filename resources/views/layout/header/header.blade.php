a69815ac4a7fe186f50c59978feab4fa3bbaa87d
Dipankar <dipankar.kataki@siqes.com>
2025-07-03 15:47:38 +0530

    create and fetch announcement list done

--- resources/views/layout/header/header.blade.php
+++ resources/views/layout/header/header.blade.php
@@ -30,20 +30,20 @@
                     </svg>
                 </div>
             </div>
-            <div class="header-search header-action-item header-action-item-hoverable text-2xl"
+            {{-- <div class="header-search header-action-item header-action-item-hoverable text-2xl"
                 data-bs-toggle="modal" data-bs-target="#dialog-search">
                 <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24"
                     height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                 </svg>
-            </div>
+            </div> --}}
         </div>
         <!-- Header Nav Start end-->
         <!-- Header Nav End start -->
         <div class="header-action header-action-end">
             <!-- Notification-->
-            <div class="dropdown">
+            {{-- <div class="dropdown">
                 <div class="dropdown-toggle" id="nav-notification-dropdown"
                     data-bs-toggle="dropdown">
                     <div class="text-2xl header-action-item header-action-item-hoverable">
@@ -234,7 +234,7 @@
                         </div>
                     </li>
                 </ul>
-            </div>
+            </div> --}}
             <!-- User Dropdown-->
             <div class="dropdown">
                 <div class="dropdown-toggle" id="user-dropdown" data-bs-toggle="dropdown">
