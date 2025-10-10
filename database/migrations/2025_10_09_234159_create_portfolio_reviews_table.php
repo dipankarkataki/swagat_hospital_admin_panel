36d877ab6d4bb2a4d52f7f9e50a1abd1da695ffb
Dipankar <dipankar.kataki@siqes.com>
2025-10-10 21:43:40 +0530

    review section added

--- database/migrations/2025_10_09_234159_create_portfolio_reviews_table.php
+++ database/migrations/2025_10_09_234159_create_portfolio_reviews_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('portfolio_reviews', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('portfolio_id');
+            $table->string('name');
             $table->text('review');
             $table->integer('rating');
             $table->boolean('status')->default(1);
